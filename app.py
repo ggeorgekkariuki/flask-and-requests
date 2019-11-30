5fd142b35712cb70bc5115ea88fb7602f071316c
ggeorgekkariuki <gconsummer.gk@gmail.com>
2019-11-30 21:59:45 +0300

    Form data method added

--- app.py
+++ app.py
@@ -6,14 +6,25 @@ app = Flask(__name__)
 def query_example():
 
     language = request.args.get('language')
-    framework = request.args.get('framework')
+    framework = request.args['framework']
     
+    return '''<h1>The language value is {}.</h1>
+              <h1> The framework is {}</h1>'''.format(language, framework)
 
-    return '''<h1>The language value is {} and framework is {}</h1>'''.format(language, framework)
-
-@app.route('/form-example')
+@app.route('/form-example', methods=['GET', 'POST'])
 def form_example():
-    return 'Todo ...'
+    if request.method == 'POST':
+        language = request.form.get('language')
+        framework = request.form['framework']
+
+        return '''<h1>The language value is {}</h1>
+        <h1> The framework is {}</h1>'''.format(language, framework)
+
+    return '''<form method="POST">
+                Language: <input type="text" name="language"><br>
+                Framework: <input type="text" name="framework"><br>
+                <input type="submit" value="Submit"><br>
+            </form>'''
 
 @app.route('/json-example')
 def json_example():
